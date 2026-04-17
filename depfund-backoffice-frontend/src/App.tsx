e8c72db53c2988d630bb1a1abecc8908252c6e0a
Valentin190306 <valentin.joel.rm@gmail.com>
2026-04-17 19:29:23 -0300

    Remove unused Dashboard and EditUserPermissions components and styles; add RoleEdit and RoleManager components with associated styles for role management functionality

--- depfund-backoffice-frontend/src/App.tsx
+++ depfund-backoffice-frontend/src/App.tsx
@@ -2,19 +2,18 @@ import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./features/auth.login/components/Login";
 import Register from "./features/auth.login/components/Register";
-import EditUserPermissions from "./features/admin.users/components/EditUserPermissions";
-import Dashboard from "./features/admin.users/components/Dashboard";
+import RoleManager from "./features/admin.users/components/RoleManager";
+import RoleEdit from "./features/admin.users/components/RoleEdit";
 
 const App: React.FC = () => {
   return (
     <Routes>
       {/* Redirige la raíz (/) al login por defecto */}
       <Route path="/" element={<Navigate to="/login" />} />
-      
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/editUserPermissions" element={<EditUserPermissions />} />      
+      <Route path="/role-manager" element={<RoleManager />} />
+      <Route path="/role-edit" element={<RoleEdit />} />      
     </Routes>
   );
 };
